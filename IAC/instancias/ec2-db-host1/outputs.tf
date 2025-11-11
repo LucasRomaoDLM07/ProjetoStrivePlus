e222e19ca92908477faeffb621d58c0f89c51f39
LucasMunhoz <lucasroh2007@gmail.com>
2025-11-11 17:19:21 -0300

    Create outputs.tf

---
+++ IAC/instancias/ec2-db-host1/outputs.tf
@@ -0,0 +1,14 @@
+output "ec2_db_host_public_ip" {
+  description = "IP público da instância DB Host"
+  value       = aws_instance.db_host.public_ip
+}
+
+output "ec2_db_host_private_ip" {
+  description = "IP privado da instância DB Host"
+  value       = aws_instance.db_host.private_ip
+}
+
+output "ec2_db_host_id" {
+  description = "ID da instância DB Host"
+  value       = aws_instance.db_host.id
+}
