6596a0f47eafb66b911ba2ec6154cb354580017e
LucasMunhoz <lucasroh2007@gmail.com>
2025-11-14 17:24:43 -0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "🚀 Iniciando aplicação..."
+
+# garante que o python3 existe
+command -v python3 >/dev/null 2>&1 || { echo "Python3 não encontrado!"; exit 1; }
+
 nohup python3 /home/ec2-user/deploy/myapp/app.py > /home/ec2-user/app.log 2>&1 &
 echo $! > /home/ec2-user/app.pid
